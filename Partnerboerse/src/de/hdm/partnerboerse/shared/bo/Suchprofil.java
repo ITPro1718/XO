5220fc80a066646c0ebc93aca59b438b3b932d84
Siam Gundermann <sg145@hdm-stuttgart.de>
2017-11-30 11:56:57 +0100

    test Für Commithistorie

--- Partnerboerse/src/de/hdm/partnerboerse/shared/bo/Suchprofil.java
+++ Partnerboerse/src/de/hdm/partnerboerse/shared/bo/Suchprofil.java
@@ -3,10 +3,12 @@ package de.hdm.partnerboerse.shared.bo;
 public class Suchprofil extends BusinessObjekt{
 
   /**
-   * 
+   * Testkommentar
    */
   private static final long serialVersionUID = 1L;
   
   private String title;
+  
+  private String test;
 
 }
