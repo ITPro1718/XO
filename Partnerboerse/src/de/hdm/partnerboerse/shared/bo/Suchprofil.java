344736b6b99562cff0a6f93298a74a66dff1e39f
Siam Gundermann <sg145@hdm-stuttgart.de>
2017-11-30 12:21:05 +0100

    Testattribute löschen

--- Partnerboerse/src/de/hdm/partnerboerse/shared/bo/Suchprofil.java
+++ Partnerboerse/src/de/hdm/partnerboerse/shared/bo/Suchprofil.java
@@ -3,18 +3,10 @@ package de.hdm.partnerboerse.shared.bo;
 public class Suchprofil extends BusinessObjekt{
 
   /**
-   * Testkommentar
+   * 
    */
   private static final long serialVersionUID = 1L;
   
   private String title;
-  
-  private String test;
-  
-  private String test1;
-  
-  private String test2;
-  
-  private String test3;
 
 }
