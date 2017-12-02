3e718523947f05b5f7af1cd2fe66caee04104913
eve94 <eve94@DESKTOP-QAF1VGS>
2017-12-02 11:44:18 +0100

    Test Commit

--- Partnerboerse/src/de/hdm/partnerboerse/shared/bo/Aehnlichkeitsmass.java
+++ Partnerboerse/src/de/hdm/partnerboerse/shared/bo/Aehnlichkeitsmass.java
@@ -3,7 +3,7 @@ package de.hdm.partnerboerse.shared.bo;
 public class Aehnlichkeitsmass extends BusinessObjekt  {
 
   /**
-   * 
+   * Testing
    */
   private static final long serialVersionUID = 1L;
 
