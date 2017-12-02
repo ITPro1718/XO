4ecb90bc8198a265138fb705a7423d363699f5d2
Siam Gundermann <sg145@hdm-stuttgart.de>
2017-12-02 13:30:40 +0100

    Impl initial erstellen

--- Partnerboerse/src/de/hdm/partnerboerse/server/db/ElementMapper.java
+++ Partnerboerse/src/de/hdm/partnerboerse/server/db/ElementMapper.java
@@ -1,6 +1,11 @@
 package de.hdm.partnerboerse.server.db;
 
 public class ElementMapper {
+
+	public static ElementMapper elementMapper() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 		
 
--- Partnerboerse/src/de/hdm/partnerboerse/server/db/FreitextMapper.java
+++ Partnerboerse/src/de/hdm/partnerboerse/server/db/FreitextMapper.java
@@ -2,4 +2,9 @@ package de.hdm.partnerboerse.server.db;
 
 public class FreitextMapper {
 
+	public static FreitextMapper freitextMapper() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
