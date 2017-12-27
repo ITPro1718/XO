05177676071a00feaaf08420b31d4d2aac0197b5
AdnanBicaj <Ado@DESKTOP-7VP388A>
2017-12-27 16:40:29 +0100

    Reports View prog start

--- Partnerboerse/src/de/hdm/partnerboerse/client/ReportGenerator.java
+++ Partnerboerse/src/de/hdm/partnerboerse/client/ReportGenerator.java
@@ -1,13 +1,29 @@
 package de.hdm.partnerboerse.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class ReportGenerator implements EntryPoint{
 
 	@Override
 	public void onModuleLoad() {
-		// TODO Auto-generated method stub
+
+		HTML html1 = new HTML("This is first GWT HTML widget using <b> tag of html.");
+		HTML html2 = new HTML("This is second GWT HTML widget using <i> tag of html.");
+
+		// use UIObject methods to set HTML widget properties.
+		html1.addStyleName("gwt-Green-Border");
+		html2.addStyleName("gwt-Blue-Border");
+
+		// add widgets to the root panel.
+		VerticalPanel panel = new VerticalPanel();
+		panel.setSpacing(10);
+		panel.add(html1);
+		panel.add(html2);
+
+		RootPanel.get("mwrap").add(panel);
 		
 	}
-
 }
