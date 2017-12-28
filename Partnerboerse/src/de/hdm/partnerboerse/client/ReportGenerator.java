ec815d0a840485cb5f5d7b4a185c96de22ea134f
AdnanBicaj <Ado@DESKTOP-7VP388A>
2017-12-28 14:05:31 +0100

    Report HTML mit Java verbunden

--- Partnerboerse/src/de/hdm/partnerboerse/client/ReportGenerator.java
+++ Partnerboerse/src/de/hdm/partnerboerse/client/ReportGenerator.java
@@ -1,8 +1,10 @@
 package de.hdm.partnerboerse.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.DockPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class ReportGenerator implements EntryPoint{
@@ -10,20 +12,39 @@ public class ReportGenerator implements EntryPoint{
 	@Override
 	public void onModuleLoad() {
 
-		HTML html1 = new HTML("This is first GWT HTML widget using <b> tag of html.");
-		HTML html2 = new HTML("This is second GWT HTML widget using <i> tag of html.");
+	      DockPanel dockPanel = new DockPanel();
+	      dockPanel.setStyleName("dockpanel");
+	      dockPanel.setSpacing(4);
+	      dockPanel.setHorizontalAlignment(DockPanel.ALIGN_CENTER);
 
-		// use UIObject methods to set HTML widget properties.
-		html1.addStyleName("gwt-Green-Border");
-		html2.addStyleName("gwt-Blue-Border");
+	      // Add text all around
+	      dockPanel.add(new HTML("This is the first north component."), 
+	      DockPanel.NORTH);
+	      dockPanel.add(new HTML("This is the first south component."), 
+	      DockPanel.SOUTH);
+	      dockPanel.add(new HTML("This is the east component."), 
+	      DockPanel.EAST);
+	      dockPanel.add(new HTML("This is the west component."), 
+	      DockPanel.WEST);
+	      dockPanel.add(new HTML("This is the second north component."), 
+	      DockPanel.NORTH);
+	      dockPanel.add(new HTML("This is the second south component"), 
+	      DockPanel.SOUTH);
 
-		// add widgets to the root panel.
-		VerticalPanel panel = new VerticalPanel();
-		panel.setSpacing(10);
-		panel.add(html1);
-		panel.add(html2);
+	      // Add scrollable text in the center
+	      HTML contents = new HTML("This is a ScrollPanel contained"
+	         +" at the center of a DockPanel. "
+	         +" small in order to see the nifty scroll bars!");
+	      ScrollPanel scroller = new ScrollPanel(contents);
+	      scroller.setSize("400px", "100px");
+	      dockPanel.add(scroller, DockPanel.CENTER);
 
-		RootPanel.get("mwrap").add(panel);
+
+	      VerticalPanel vPanel = new VerticalPanel();
+	      vPanel.add(dockPanel);
+
+	      // Add the widgets to the root panel.
+	      RootPanel.get("mwrapREP").add(vPanel);
 		
 	}
 }
