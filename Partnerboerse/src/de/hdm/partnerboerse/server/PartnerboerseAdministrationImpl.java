21f052cab481a6b288362d1dad8478415d9769a2
simonburghardt <sb234@hdm-stuttgart.de>
2018-01-03 14:58:09 +0100

    getSuchProfilErgebnisse Methode verbessert/verändert

--- Partnerboerse/src/de/hdm/partnerboerse/server/PartnerboerseAdministrationImpl.java
+++ Partnerboerse/src/de/hdm/partnerboerse/server/PartnerboerseAdministrationImpl.java
@@ -238,6 +238,7 @@ public class PartnerboerseAdministrationImpl extends RemoteServiceServlet
 		s.setKoerpergroesse(kgr);
 		s.setTitle(titel);
 		// !!! Für Religion fehlt das Attribut im relationalen Modell
+		// TODO: Datenbank updaten!!
 		s.setReligion(religion);
 		s.setRaucher(raucher);
 		s.setAlter(alter);
@@ -287,44 +288,44 @@ public class PartnerboerseAdministrationImpl extends RemoteServiceServlet
 
 	@Override
 	public ArrayList<Profil> getSuchProfilErgebnisse(Suchprofil suchprofil) throws IllegalArgumentException {
-		// TODO Auto-generated method stub
+
 		Suchprofil suchprofile = this.sMapper.getSuchprofil(suchprofil);
 		ArrayList<Profil> profile = this.pMapper.findAllProfiles();
 		Profil suchprofilowner = this.pMapper.findProfilByKey(suchprofile.getEigenprofilID());
 		ArrayList<Kontaktsperre> kontaktsperrenofsuchprofilowner = this.kMapper.findKontaktsperrenOf(suchprofilowner);
-		ArrayList<Integer> ids = new ArrayList<>();
 		ArrayList<Integer> fpids = new ArrayList<>();
-	
-	
-			//Bevor der Abgleich stattfindet, müssen ALLE Kontaktsperren fpids in der Arraylist fpids vorhanden sein.	
-		for(int j =0; j<kontaktsperrenofsuchprofilowner.size(); j++){
-			Kontaktsperre k = kontaktsperrenofsuchprofilowner.get(j);
+			
+		
+		// Bevor der Abgleich stattfindet, müssen ALLE Kontaktsperren fpids in der Arraylist fpids vorhanden sein.	
+		for (Kontaktsperre k : kontaktsperrenofsuchprofilowner){
 			int fpid = k.getFremdprofilID();
 			fpids.add(fpid);
-			}
-		//Abspeichern der Profil id, mit jedem Durchgang eine neue id.
-		          for (int i= 0; i < profile.size(); i++){
-					Profil p = profile.get(i);
-					int id = p.getId();
-					ids.add(id);
-					//Vergleich der id am Index i mit der kompletten fpid Arraylist.
-						if(fpids.contains(ids.get(i))){
-							profile.set(i, null);
-						}
-						//Falls die Profil id am aktuellen Index i, der Profil id des "Suchprofilbesitzers" entspricht,
-						//wird das Element an der Stelle i null gesetzt, der garbage collector löscht das referenzlose Objekt um neuen Speicher zu schaffen.
-						else if (ids.get(i)== suchprofilowner.getId()){
-								profile.set(i, null);
-						}
-							// Die Methode compare gleicht das Suchprofil mit dem Profil am aktuellen Index i ab, falls es nicht gleich ist, wird das Element in Arraylist profile am Index i null gesetzt.
-							// suchprofile bzw. das Suchprofil-Businessobjekt dient hierbei nur als Hilfsklasse, dass die Methde aufgerufen werden kann, die Methode kann natürlich auch an einer anderen Stelle oder in einer eigenen Hilfsklasse implementiert werden.
-					else if (suchprofile.compare(suchprofile, p) == false){
-								profile.set(i, null);
-							}
-					}
-				
+		}	
 		
-	
+		
+		// Abspeichern der Profil id, mit jedem Durchgang eine neue id.
+		for (Profil p : profile){
+			int id = p.getId();
+				
+			// Wenn eine FremdID in der fpID Liste ist (--> ein geblockter User), wird das Profil aus der
+			// profile-Liste gelöscht.
+			if(fpids.contains(id)){
+				profile.remove(p);
+			}
+			// Wenn die id, die des Suchprofilowners ist, wird das Profil aus der profile-Liste gelöscht.
+			else if (id == suchprofilowner.getId()){
+				profile.remove(p);
+			}
+						
+			// Die Methode compare gleicht die Anforderungen des Suchprofils mit den realen Werten aus
+			// dem Profil ab. z.B. Suchprofil-Haarfarbe = blonde, Profil-Haarfarbe = schwarz ergibt
+			// keinen Treffer.
+			// TODO: Compare Methode implementieren (Applikationslogik!!)
+			else if (suchprofile.compare(suchprofile, p) == false){
+				profile.remove(p);
+			}
+		}
+				
 		return profile;
 }
 
@@ -344,6 +345,7 @@ public class PartnerboerseAdministrationImpl extends RemoteServiceServlet
 		i.setEigenprofilID(p.getId());
 		
 		//this.iMapper.insertInfo(i); insert-Methode fehlt in Mapper klasse, Update-Methode auch (falls Eigenschaften gelöscht werden ändert sich das Infoobjekt etc.)
+		// TODO: Methode einfügen
 	}
 
 	@Override
