1672a32aa4e29d890541aa51ca0067515e221b4a
EvelynHettmann <eh031@hdm-stuttgart.de>
2018-01-14 18:42:44 +0100

    Hinzufügen der Login-Dateien ohne Implemeniertung in Editor, bearbeiten der web.xml mit login servlet

---
+++ Partnerboerse/src/de/hdm/partnerboerse/client/LoginServiceImpl.java
@@ -0,0 +1,31 @@
+package de.hdm.partnerboerse.client;
+
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import de.hdm.partnerboerse.client.LoginService;
+import de.hdm.partnerboerse.client.LoginInfo;
+import com.google.appengine.api.users.UserServiceFactory;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+public class LoginServiceImpl extends RemoteServiceServlet implements
+LoginService {
+
+public LoginInfo login(String requestUri) {
+UserService userService = UserServiceFactory.getUserService();
+User user = userService.getCurrentUser();
+LoginInfo loginInfo = new LoginInfo();
+
+if (user != null) {
+  loginInfo.setLoggedIn(true);
+  loginInfo.setEmailAddress(user.getEmail());
+  loginInfo.setNickname(user.getNickname());
+  loginInfo.setLogoutUrl(userService.createLogoutURL(requestUri));
+} else {
+  loginInfo.setLoggedIn(false);
+  loginInfo.setLoginUrl(userService.createLoginURL(requestUri));
+}
+return loginInfo;
+}
+
+}
