d529e6c168f847a4df9a7e5b03af7797951f684f
EvelynHettmann <eh031@hdm-stuttgart.de>
2018-01-29 16:43:39 +0100

    Editor aufgeräumt, AddButton etc. entfernt

--- Partnerboerse/src/de/hdm/partnerboerse/client/Editor.java
+++ Partnerboerse/src/de/hdm/partnerboerse/client/Editor.java
@@ -1,22 +1,13 @@
 package de.hdm.partnerboerse.client;
 
-import java.util.ArrayList;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -26,229 +17,133 @@ import de.hdm.partnerboerse.shared.bo.Profil;
 
 public class Editor implements EntryPoint {
 
-  private final PartnerboerseAdministrationAsync partnerAdmin =
-      GWT.create(PartnerboerseAdministration.class);
-  private final LoginServiceAsync loginService = GWT.create(LoginService.class);
+	private final PartnerboerseAdministrationAsync partnerAdmin = GWT.create(PartnerboerseAdministration.class);
+	private final LoginServiceAsync loginService = GWT.create(LoginService.class);
 
-  // Loginattribute
-  private LoginInfo loginInfo = null;
-  
-  private VerticalPanel loginPanel = new VerticalPanel();
-  private Label loginLabel =
-      new Label("Melde dich mit deinem Google-Konto an, dann kann es schon losgehen!");
-  private Anchor signInLink = new Anchor("anmelden");
+	// Loginattribute
+	private LoginInfo loginInfo = null;
 
-  // Unnötige Attribute?! Muss angepasst werden
-  private VerticalPanel mainPanel = new VerticalPanel();
-  private FlexTable profilFlexTable = new FlexTable();
-  private HorizontalPanel addPanel = new HorizontalPanel();
-  private TextBox newSymbolTextBox = new TextBox();
-  private Button addProfilButton = new Button("Add");
-  private Label lastUpdatedLabel = new Label();
+	private VerticalPanel loginPanel = new VerticalPanel();
+	private Label loginLabel = new Label("Melde dich mit deinem Google-Konto an, dann kann es schon losgehen!");
+	private Anchor signInLink = new Anchor("anmelden");
 
-  Navigation nav = new Navigation();
-  EigenProfilView epv = new EigenProfilView();
-  EditProfile ep = new EditProfile();
-  EditSuchprofil sp = new EditSuchprofil();
+	// Unnötige Attribute?! Muss angepasst werden
+	private VerticalPanel mainPanel = new VerticalPanel();
+	private TextBox newSymbolTextBox = new TextBox();
 
-  @Override
-  public void onModuleLoad() {
-    // Check login status using login service.
-    loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
-      public void onFailure(Throwable error) {}
+	Navigation nav = new Navigation();
+	EigenProfilView epv = new EigenProfilView();
+	EditProfile ep = new EditProfile();
+	EditSuchprofil sp = new EditSuchprofil();
 
-      public void onSuccess(LoginInfo result) {
-        loginInfo = result;
-        ClientSideSettings.setLoginInfo(loginInfo);
-        if (loginInfo.isLoggedIn()) {
+	@Override
+	public void onModuleLoad() {
+		// Check login status using login service.
+		loginService.login(GWT.getHostPageBaseURL(), new AsyncCallback<LoginInfo>() {
+			public void onFailure(Throwable error) {
+			}
 
-          // loadXO();
-          hasProfile(result);
+			public void onSuccess(LoginInfo result) {
+				loginInfo = result;
+				ClientSideSettings.setLoginInfo(loginInfo);
+				if (loginInfo.isLoggedIn()) {
 
-        } else {
-          loadLogin();
-        }
-      }
-    });
-  }
+					// loadXO();
+					hasProfile(result);
 
-  public void loadXO() {
+				} else {
+					loadLogin();
+				}
+			}
+		});
+	}
 
-    // Navigation Area
-    RootPanel.get("navwrap").add(nav);
+	public void loadXO() {
 
-    // Profile Edit - Panel wird erzeugt und eingefügt.
-    // HTMLPanel editProfilePanel = new HTMLPanel(
-    // "<h3>" + "Hier können Sie ihre Profilinformationen bearbeiten." +
-    // "</h3>");
-    // editProfilePanel.add(ep);
-    //
-    // RootPanel.get("contwrap").add(editProfilePanel);
+		// Navigation Area
+		RootPanel.get("navwrap").add(nav);
 
-    // Search Profile
+		// Associate the Main panel with the HTML host page.
+		RootPanel.get("contwrap").add(mainPanel);
 
-    // HTMLPanel spPanel = new HTMLPanel("<h3>" + "Hier können Sie Ihr
-    // Suchprofil erstellen." + "</h3>");
-    // spPanel.add(sp);
-    //
-    // RootPanel.get("contwrap").add(spPanel);
+		// Associate the Main panel with the HTML host page.
+		RootPanel.get("contwrap").add(epv);
 
-    // Create table for Profil
-    profilFlexTable.setText(0, 0, "Vorname");
-    profilFlexTable.setText(0, 1, "Nachname");
-    profilFlexTable.setText(0, 2, "Haarfarbe");
-    profilFlexTable.setText(0, 3, "Körpergröße");
-    profilFlexTable.setText(0, 4, "Raucher");
-    profilFlexTable.setText(0, 5, "Religion");
-    profilFlexTable.setText(0, 6, "Geburtsdatum");
-    profilFlexTable.setText(0, 7, "Passwort");
-    profilFlexTable.setText(0, 8, "Email");
+		// Move cursor focus to the input box.
+		newSymbolTextBox.setFocus(true);
 
-    // Assemble Add Stock panel.
-    addPanel.add(newSymbolTextBox);
-    addPanel.add(addProfilButton);
+		/*
+		 * Neues Button Widget erzeugen und eine Beschriftung festlegen.
+		 */
+		VerticalPanel navPanel = new VerticalPanel();
 
-    // Assemble Main panel.
-    mainPanel.add(profilFlexTable);
-    mainPanel.add(addPanel);
-    mainPanel.add(lastUpdatedLabel);
+		RootPanel.get("contwrap").add(navPanel);
 
+	}
 
+	private void getProfilesFromServer() {
+		partnerAdmin.getProfilByEmail(loginInfo.getEmailAddress(), new AsyncCallback<Profil>() {
 
-    // Associate the Main panel with the HTML host page.
-    RootPanel.get("contwrap").add(mainPanel);
-    
-    // Associate the Main panel with the HTML host page.
-    RootPanel.get("contwrap").add(epv);
-        
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("Das Laden des Profils ist fehlgeschlagen!");
 
-    // Move cursor focus to the input box.
-    newSymbolTextBox.setFocus(true);
+			}
 
-    /*
-     * Neues Button Widget erzeugen und eine Beschriftung festlegen.
-     */
-    final Button findProfilButton = new Button("Finde Profile");
-    VerticalPanel navPanel = new VerticalPanel();
+			@Override
+			public void onSuccess(Profil result) {
 
-    RootPanel.get("contwrap").add(navPanel);
-    navPanel.add(findProfilButton);
+				ClientSideSettings.setProfil(result);
+				loadXO();
+			}
+		});
+	}
 
-    addProfilButton.addClickHandler(new ClickHandler() {
+	private void loadLogin() {
+		// Assemble login panel.
+		signInLink.setHref(loginInfo.getLoginUrl());
+		loginPanel.add(loginLabel);
+		loginPanel.add(signInLink);
+		RootPanel.get("contwrap").add(loginPanel);
+	}
 
-      @Override
-      public void onClick(ClickEvent event) {
-        // TODO Auto-generated method stub
+	private void hasProfile(final LoginInfo result) {
 
-        getProfileFromServer();
+		loginInfo = result;
 
-      }
+		loginService.getEmailFromProfil(result.getEmailAddress(), new AsyncCallback<Boolean>() {
 
-      private void getProfileFromServer() {
+			@Override
+			public void onFailure(Throwable caught) {
+				Window.alert("Daten konnten nicht geladen werden.");
+			}
 
-        partnerAdmin.getAllProfils(new AsyncCallback<ArrayList<Profil>>() {
+			@Override
+			public void onSuccess(Boolean isResult) {
+				if (isResult) {
+					getProfilesFromServer();
+				} else {
+					loadCreateEigenProfil(loginInfo);
+				}
 
-          @Override
-          public void onFailure(Throwable caught) {
-            // Show the RPC error message to the user
-          }
+			}
 
-          @Override
-          public void onSuccess(ArrayList<Profil> result) {
-            for (Profil p : result) {
-              addToTable(p);
-            }
-          }
-        });
-      }
+			private void loadCreateEigenProfil(LoginInfo result) {
 
-      private void addToTable(Profil p) {
-        int row = profilFlexTable.getRowCount();
-        profilFlexTable.setText(row, 0, p.getNachname());
-        profilFlexTable.setText(row, 1, p.getVorname());
-        profilFlexTable.setText(row, 2, p.getHaarfarbe());
-        int kgr = (int) p.getKoerpergroesse();
-        String s = "" + kgr;
-        profilFlexTable.setText(row, 3, s);
-        profilFlexTable.setText(row, 4, "" + p.isRaucher());
-        profilFlexTable.setText(row, 5, p.getReligion());
-        profilFlexTable.setText(row, 6, "" + p.getGeburtsdatum());
-        profilFlexTable.setText(row, 8, p.getEmail());
+				CreateEigenProfil cep = new CreateEigenProfil();
+				cep.setLoginInfo(result);
 
-        // profilFlexTable.setWidget(row, 2, new Label());
-        // profilFlexTable.getCellFormatter().addStyleName(row, 1,
-        // "watchListNumericColumn");
-        // profilFlexTable.getCellFormatter().addStyleName(row, 2,
-        // "watchListNumericColumn");
-        // profilFlexTable.getCellFormatter().addStyleName(row, 3,
-        // "watchListRemoveColumn");
-      }
-    });
+				HTMLPanel createEigenProfilPanel = new HTMLPanel(
+						"<h3>" + "Hier können Sie ihre Profil erstellen." + "</h3>");
 
-  }
+				createEigenProfilPanel.add(cep);
 
-  private void getProfilesFromServer() {
-    partnerAdmin.getProfilByEmail(loginInfo.getEmailAddress(), new AsyncCallback<Profil>() {
+				RootPanel.get("contwrap").clear();
+				RootPanel.get("contwrap").add(createEigenProfilPanel);
 
-      @Override
-      public void onFailure(Throwable caught) {
-        // TODO Auto-generated method stub
-        
-      }
+			}
+		});
 
-      @Override
-      public void onSuccess(Profil result) {
-        
-        ClientSideSettings.setProfil(result);
-        loadXO();
-      }});
-    }
-
-  private void loadLogin() {
-    // Assemble login panel.
-    signInLink.setHref(loginInfo.getLoginUrl());
-    loginPanel.add(loginLabel);
-    loginPanel.add(signInLink);
-    RootPanel.get("contwrap").add(loginPanel);
-  }
-
-  private void hasProfile(final LoginInfo result) {
-
-    loginInfo = result;
-    
-    loginService.getEmailFromProfil(result.getEmailAddress(), new AsyncCallback<Boolean>() {
-
-      @Override
-      public void onFailure(Throwable caught) {
-        Window.alert("Daten konnten nicht geladen werden.");
-      }
-
-      @Override
-      public void onSuccess(Boolean isResult) {
-        if (isResult) {
-          getProfilesFromServer();
-        } else {
-          loadCreateEigenProfil(loginInfo);
-        }
-
-      }
-
-      private void loadCreateEigenProfil(LoginInfo result) {
-
-        CreateEigenProfil cep = new CreateEigenProfil();
-        cep.setLoginInfo(result);
-
-        HTMLPanel createEigenProfilPanel =
-            new HTMLPanel("<h3>" + "Hier können Sie ihre Profil erstellen." + "</h3>");
-
-        createEigenProfilPanel.add(cep);
-
-        RootPanel.get("contwrap").clear();
-        RootPanel.get("contwrap").add(createEigenProfilPanel);
-
-      }
-    });
-
-  }
+	}
 
 }
