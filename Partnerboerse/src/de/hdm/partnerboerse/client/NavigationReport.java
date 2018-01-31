3824c4256dccb3170103fbd5105a41fc8fb69087
AdnanBicaj <adnan.bicaj@gmx.de>
2018-01-31 19:02:34 +0100

    Feintuning Frontend

--- Partnerboerse/src/de/hdm/partnerboerse/client/NavigationReport.java
+++ Partnerboerse/src/de/hdm/partnerboerse/client/NavigationReport.java
@@ -19,21 +19,16 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class NavigationReport extends VerticalPanel {
 
 	LoginInfo loginInfo = ClientSideSettings.getLoginInfo();
-	
+
 	/*
 	 * Navigation zeichnen und bereitstellen
 	 */
-	final Button meinProfil = new Button("Mein Profil");
-	final Button meineMerkliste = new Button("Merkliste");
-	final Button meineKontaktsperren = new Button("Kontaktsperren");
-	final Button meineSuchprofile = new Button("Suchprofile");
-
-	HTML reports = new HTML("<h3>" + "REPORTS" + "</h3>");
-
-	final Button meineSuchprofilReports = new Button("Suchprofil Report");
-	final Button meinePartnervorschlaege = new Button("Partnervorschläge");
-
+	final Button meinProfil = new Button("Zurück zum Profil");
+	final Button Reports = new Button("Reports");
 	final Anchor logoutUser = new Anchor("Abmelden");
+	
+	HTML reports = new HTML("<h3>" + "REPORTS" + "</h3>");
+	
 	@Override
 	public void onLoad() {
 
@@ -42,18 +37,13 @@ public class NavigationReport extends VerticalPanel {
 
 		ppic.setTitle("Profilbild");
 		ppic.addStyleName("ppic");
-		meinProfil.setTitle("Mein Profil");
+		meinProfil.setTitle("Zurück zum Profil");
 		meinProfil.addStyleName("button");
-		meineMerkliste.setTitle("Merkliste");
-		meineMerkliste.addStyleName("button");
-		meineKontaktsperren.setTitle("Kontaktsperre");
-		meineKontaktsperren.addStyleName("button");
-		meineSuchprofile.setTitle("Suchprofile");
-		meineSuchprofile.addStyleName("button");
-		meineSuchprofile.setTitle("Suchprofil Report");
-		meineSuchprofile.addStyleName("button");
-		meineSuchprofile.setTitle("Partnervorschläge");
-		meineSuchprofile.addStyleName("button");
+
+		Reports.addStyleName("button");
+		Reports.setTitle("Partnervorschläge");
+		Reports.addStyleName("button");
+
 		logoutUser.setTitle("Abmelden");
 		logoutUser.addStyleName("offbutton");
 
@@ -61,114 +51,41 @@ public class NavigationReport extends VerticalPanel {
 		this.add(navi);
 		navi.add(ppic);
 		navi.add(meinProfil);
-		navi.add(meineMerkliste);
-		navi.add(meineKontaktsperren);
-		navi.add(meineSuchprofile);
-		navi.add(reports);
-		navi.add(meineSuchprofilReports);
-		navi.add(meinePartnervorschlaege);
+		navi.add(Reports);
 		navi.add(logoutUser);
-		
-	     
+
 		/*
 		 * Methode lädt die View des eigenen Profils
 		 */
 		meinProfil.addClickHandler(new ClickHandler() {
-          
-          @Override
-          public void onClick(ClickEvent event) {
-            
-            Window.Location.assign("Partnerboerse.html");
 
-          	}
-          });
-		
-	      /*
-         * Methode lädt die View der Merkliste
-         */
-		meineMerkliste.addClickHandler(new ClickHandler() {
-          
-          @Override
-          public void onClick(ClickEvent event) {
-        	  
-        	Window.Location.assign("Partnerboerse.html");
-            
-/*            EditMerkliste em = new EditMerkliste();
-            
-            HTMLPanel emPanel = new HTMLPanel("<h3>" + "Hier können Sie ihre Merkliste editieren" + "</h3>");
-            emPanel.add(em);
-            
-            RootPanel.get("contwrap").clear();
-            RootPanel.get("contwrap").add(emPanel);
-            */
-          }
-        });
-		
-		/*
-		 * Methode lädt die View der Kontaktsperren
-		 */
-		meineKontaktsperren.addClickHandler(new ClickHandler() {
-          
-          @Override
-          public void onClick(ClickEvent event) {
-            
-        	Window.Location.assign("Partnerboerse.html");
-        	  
-/*            EditKontaktsperre ep = new EditKontaktsperre();
-            
-            HTMLPanel epPanel = new HTMLPanel("<h3>" + "Hier können Sie ihre Kontaktsperren editieren" + "</h3>");
-            epPanel.add(ep);
-            
-            RootPanel.get("contwrap").clear();
-            RootPanel.get("contwrap").add(epPanel);*/
-          }
-        });
-		
-		meineSuchprofile.addClickHandler(new ClickHandler() {
-          
-          @Override
-          public void onClick(ClickEvent event) {
-              
-          Window.Location.assign("Partnerboerse.html");
-          
-      
-           /*ListViewSuchProfil sp = new ListViewSuchProfil();
-            
-            HTMLPanel spPanel = new HTMLPanel("<h3>" + "Hier können Sie Ihr Suchprofil erstellen." + "</h3>");
-            spPanel.add(sp);
-
-            RootPanel.get("contwrap").clear();
-            RootPanel.get("contwrap").add(spPanel);*/
-            
-          }
-        });
-		
-		logoutUser.addClickHandler(new ClickHandler() {
-			
 			@Override
 			public void onClick(ClickEvent event) {
-					  
+
+				Window.Location.assign("Partnerboerse.html");
+
+			}
+		});
+
+		Reports.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+
+
+					Window.Location.assign("PartnerboerseReport.html");
+
+				}
+			});
+
+		logoutUser.addClickHandler(new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+
 				ClientSideSettings.getLoginInfo();
 				logoutUser.setHref(ClientSideSettings.getLoginInfo().getLogoutUrl());
-				
-			}
-		});
-		
-		meineSuchprofilReports.addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				Window.Location.assign("PartnerboerseReport.html");
-				
-			}
-		});
-		
-		meinePartnervorschlaege.addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent event) {
-				Window.Location.assign("PartnerboerseReport.html");
-				
+
 			}
 		});
 
